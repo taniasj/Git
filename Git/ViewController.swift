33b60b7b1c4b11737e96a4f493cc92af00a34435
Tania <tania@macbook-pro-de-luis-2.local>
2017-12-15 15:16:27 -0600

    comentarios hola

--- Git/ViewController.swift
+++ Git/ViewController.swift
@@ -12,12 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //hola
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //hola
     }
 
 
